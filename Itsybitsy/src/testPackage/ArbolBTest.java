c43f4ec1545275f3e1a482452e11653287374731
tian2992 <tian2992@da640d5e-38f4-083c-ef32-2bf749149446>
2010-05-09 01:18:12 +0000

    Añadido paquete de clases de prueba

---
+++ Itsybitsy/src/testPackage/ArbolBTest.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArbolBTest extends TestCase {
+    public ArbolBTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
