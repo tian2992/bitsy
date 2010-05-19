56cc42f6708b97adc99a2e17ed281d2cbf118d48
tian2992 <tian2992@da640d5e-38f4-083c-ef32-2bf749149446>
2010-05-19 05:31:35 +0000

    Inicio del primer thread, documentacion en NetWorkController, recepcion de archivos

--- trunk/Itsybitsy/src/BitsyL/NetworkController.java
+++ trunk/Itsybitsy/src/BitsyL/NetworkController.java
@@ -6,21 +6,48 @@ import java.net.*;
 
 import listaEnlazada.ListaEnlazada;
 
-public class NetworkController{
+
+/**
+ * Primero se obtiene la instancia del Network Controller, con NetworkController.getInstance
+ * luego se le da init();
+ * luego se pide el socket (con getSocket(InetAddress) o con List &lt;Socket&gt; getSockets)
+ * luego se crea un objeto de tipo GetIndex o cualquiera de las acciones Thread
+ * 
+ * se da un {nombreDelObjetoThread}.join(200) o una cantidad de milisegundos para que responda
+ * y se obtiene del objeto el resultado depende del objeto
+ * 
+ */
+public class NetworkController implements Runnable{
 
     ServerSocket serverSocket = null;
     PrintWriter out = null;
     BufferedReader in = null;
 
     boolean listening = true;
+    static boolean iniciado = false;
     
-    ListaEnlazada<NetWorkControllerThread> netControllers = new ListaEnlazada<NetWorkControllerThread>();
-    
+    ListaEnlazada<Socket> sockets = new ListaEnlazada<Socket>();
+
+
+    /**
+     * @return
+     */
+    public static boolean init(){
+        if (iniciado){
+            return true;
+        }
+        else{
+            NetworkController.getInstance().run();
+            iniciado = true;
+            return false;
+        }
+    }
 
     public NetworkController() {
         super();
-        
-        boolean listening = true;
+    }
+    
+    public void run(){
         try {
             serverSocket = new ServerSocket(1992);
         } catch (IOException e) {
@@ -29,16 +56,37 @@ public class NetworkController{
         }
         try {
             while (listening) {
-                NetWorkControllerThread t = new NetWorkControllerThread(serverSocket.accept());
-                t.start();
-                netControllers.add(t);
+                sockets.add(serverSocket.accept());
             }
+            
+            serverSocket.close();
+            
         } catch (Exception e){
             e.printStackTrace();
         }
     }
+    
+    public Socket getSocket(InetAddress ia){
+        for (Socket s: sockets){
+            if (s.getInetAddress().equals(ia))
+                return s;
+        }
+        return null;
+    }
+    
+    
+    
+    public static NetworkController getInstance() {
+        return NetworkControllerHolder.INSTANCE;
+    }
 
+    public void setSockets(ListaEnlazada<Socket> sockets) {
+        this.sockets = sockets;
+    }
 
+    public ListaEnlazada<Socket> getSockets() {
+        return sockets;
+    }
 
     private static class NetworkControllerHolder{
         private static final NetworkController INSTANCE = new NetworkController();
